195d190ba66e8c41413b8f3b8dfd56e5e8b7f907
AdrSpeer <adn.speer@gmail.com>
2024-02-18 12:13:11 +0100

    added new features

--- main.js
+++ main.js
@@ -6,52 +6,63 @@ let questions = [
     question: "Was ist die Hauptstadt von Deutschland?",
     answers: ["Bonn", "Düsseldorf", "Berlin", "Hamburg"],
     correctAnswer: 2,
+    img: "assets/img/berlin.jpg",
   },
   {
     question: "Was ist die Hauptstadt von Spanien?",
     answers: ["Barcelona", "Sevilla", "Grenada", "Madrid"],
     correctAnswer: 3,
+    img: "assets/img/madrid.jpg",
   },
   {
     question: "Was ist die Hauptstadt von Bosnien-Herzegowina?",
     answers: ["Banja-Luka", "Mostar", "Sarajevo", "Belgrad"],
     correctAnswer: 2,
+    img: "assets/img/sarajevo.jpg",
   },
   {
     question: "Was ist die Hauptstadt von Schweden?",
     answers: ["Stockholm", "Helsinki", "Malmö", "Oslo"],
     correctAnswer: 0,
+    img: "assets/img/stockholm.jpg",
   },
   {
     question: "Was ist die Hauptstadt von Argentinien?",
     answers: ["Rosario", "Buenos Aires", "Cordoba", "Medoza"],
     correctAnswer: 1,
+    img: "assets/img/buenos.jpg",
   },
   {
     question: "Was ist die Hauotstadt von Brasilien?",
-    answers: ["Rio de Janeiro", "Bahia", "Brasilia", "Puerto Alegre"],
+    answers: ["Rio de Janeiro", "Bahia", "Brasilia", "Santos"],
     correctAnswer: 2,
+    img: "assets/img/brasilia.jpg",
   },
   {
     question: "Was ist die Hauotstadt von Marokko?",
     answers: ["Marrakesch", "Rabat", "Casablanca", "Fes"],
     correctAnswer: 1,
+    img: "assets/img/rabat.jpg",
   },
   {
     question: "Was ist die Hauotstadt von Südafrika?",
     answers: ["Kapstadt", "Johannesburg", "Durban", "Pretoria"],
     correctAnswer: 3,
+    img: "assets/img/pretoria.jpg",
   },
 ];
 
 let answersDiv = document.getElementById("answers");
 let answerButton = document.querySelector(".answer-button");
+let checkResult = document.querySelector(".check-result");
 
 function displayQuestion() {
   if (currentQuestionIndex >= questions.length) {
     document.getElementById("quiz-container").hidden = true;
     document.getElementById("result").hidden = false;
-    document.getElementById("score").textContent = score;
+    document.getElementById(
+      "score"
+    ).textContent = `Du hast ${score} von 8 möglichen Punkten erhalten`;
     return;
   }
 
@@ -62,7 +73,7 @@ function displayQuestion() {
 
   question.answers.forEach((answer, index) => {
     let answerButton = document.createElement("button");
-    answerButton.classList.add("btn", "btn-dark", "me-2");
+    answerButton.classList.add("btn", "btn-dark", "me-1", "fs-6", "p-0");
     answerButton.textContent = answer;
     answerButton.onclick = () => checkAnswer(index);
     answersDiv.appendChild(answerButton);
@@ -72,15 +83,26 @@ displayQuestion();
 
 function checkAnswer(userAnswer) {
   let correctAnswer = questions[currentQuestionIndex].correctAnswer;
-
+  let imgSrc = questions[currentQuestionIndex].img;
+  let correctAnswerOutput =
+    questions[currentQuestionIndex].answers[correctAnswer];
   if (userAnswer == correctAnswer) {
     score += 1;
+    checkResult.innerHTML = ` <img style="max-width: 50%; max-height: 70%" src='${imgSrc}' alt='Bild'>
+    <h2> Super! ${correctAnswerOutput} ist richtig </h2>
+    `;
+    answersDiv.hidden = true;
+  } else {
+    checkResult.innerHTML = ` <img style="max-width: 50%; max-height: 70%" src='${imgSrc}' alt='Bild'>
+    <h2> Das ist leider falsch! ${correctAnswerOutput} ist richtig </h2>
+    `;
+    answersDiv.hidden = true;
   }
-
-  nextQuestion();
 }
 
 function nextQuestion() {
+  checkResult.innerHTML = "";
+  answersDiv.hidden = false;
   currentQuestionIndex += 1;
   displayQuestion();
 }
